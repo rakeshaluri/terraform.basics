30bb8566dd8f77d5c9e514eb544bb4ae3c7b3b4d
rakesh <cloudawsrakesh@gmail.com>
2022-10-17 01:10:26 +0100

    demo of terrafrom

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -1,12 +1,11 @@
 variable "sample" {
-  
-  default     = "Hello World"
-  
+    default     = "Hello World"
 }
 
 output "sample-op" {
   value       = var.sample  
 }
+
 output "sample-op1" {
   value       = "Value is $(var.sample)"  
 }
